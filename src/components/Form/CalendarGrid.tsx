587b3bfdb2f4701b1c9f9a02d65ae0ea38d35ed0
Dale Webster <dalewebster48@gmail.com>
2025-08-25 21:06:16 +0100

    Stuff

--- src/components/Form/CalendarGrid.tsx
+++ src/components/Form/CalendarGrid.tsx
@@ -4,7 +4,6 @@ import styles from './CalendarGrid.module.css'
 export interface CalendarGridProps {
     onDateRangeChange: (startDate: string, endDate: string) => void
     label?: string
-    clearTrigger?: number // Increment this to trigger a clear
 }
 
 interface CalendarDay {
@@ -40,14 +39,6 @@ function CalendarGrid(props: CalendarGridProps) {
             return () => document.removeEventListener('mousedown', handleClickOutside)
         }
     }, [showMonthDropdown])
-    
-    // Clear calendar when clearTrigger changes
-    useEffect(() => {
-        if (props.clearTrigger !== undefined && props.clearTrigger > 0) {
-            setStartDate(null)
-            setEndDate(null)
-        }
-    }, [props.clearTrigger])
     
     // Call the callback directly when dates change, not in useEffect
     const updateDateRange = (start: Date | null, end: Date | null) => {
